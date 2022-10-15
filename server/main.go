c6b5db1d2e592a99ebf893e2812fab09b9e3ca36
rohitchauraisa1997 <rohit.chaurasia997@gmail.com>
2022-10-16 02:18:00 +0900

    main.go

---
+++ server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/rohitchauraisa1997/calorie-tracker/routes"
+)
+
+func main() {
+	fmt.Println("Running calorie-tracker")
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8001"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(cors.Default())
+
+	router.POST("/entry/add", routes.AddEntry)
+	router.GET("/entries", routes.GetEntries)
+	router.GET("/entry", routes.GetEntryById)
+	router.GET("/ingredient", routes.GetEntriesByIngredient)
+
+	router.PUT("/entry/update", routes.UpdateEntry)
+	router.PUT("/ingredient/update", routes.UpdateIngredients)
+	router.DELETE("/entry/softdelete", routes.SoftDeleteEntry)
+	router.DELETE("/entry/delete", routes.DeleteEntry)
+
+	router.Run(":" + port)
+}
