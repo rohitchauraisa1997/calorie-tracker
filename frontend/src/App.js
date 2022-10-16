2d6dbba34a218caa4e8c80713388b4556fda80fc
rohitchauraisa1997 <rohit.chaurasia997@gmail.com>
2022-10-16 13:06:39 +0900

    frontend react app

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css'
+import Entries from './components/entries.components'
+
+function App() {
+  return (
+    <div>
+      <Entries />
+    </div>
+  );
+}
+
+export default App;
